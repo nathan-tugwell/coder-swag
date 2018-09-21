dbdbe70e73b000a99375916144f0f2be0523d65f
Nathan <ntugwell@petsathome.co.uk>
2018-09-21 08:54:20 +0100

    Lesson-One building the interface with TableView

---
+++ coder-swag/View/CategoryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryTableViewCell.swift
+//  coder-swag
+//
+//  Created by Nathan Tugwell on 21/09/2018.
+//  Copyright © 2018 petsathome. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
