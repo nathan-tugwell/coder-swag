d37858b0680b921660f4ca32cea869e2be38bb83
Nathan <ntugwell@petsathome.co.uk>
2018-09-24 08:10:59 +0100

    CollectionView added

---
+++ coder-swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Nathan Tugwell on 21/09/2018.
+//  Copyright © 2018 petsathome. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
