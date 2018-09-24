a02ee26b39c2d804f279c40e6a22221144e22050
Nathan <ntugwell@petsathome.co.uk>
2018-09-24 09:36:07 +0100

    CollectionView Data added

--- coder-swag/Controller/CategoriesViewController.swift
+++ coder-swag/Controller/CategoriesViewController.swift
@@ -32,6 +32,23 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
             return CategoryTableViewCell()
         }
     }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataServices.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductSegue", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsViewController {
+            let barButton = UIBarButtonItem()
+            barButton.title = ""
+            navigationItem.backBarButtonItem = barButton
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+            
+        }
+    }
 
 }
 
